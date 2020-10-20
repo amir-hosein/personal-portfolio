c9fe02187267585bd7980f26db2f7221658af311
Amirhosein <a.ghahremani79@yahoo.com>
2020-10-20 13:04:26 +0200

    login, auth, database

--- routes/index.js
+++ routes/index.js
@@ -7,30 +7,79 @@
 
 const express = require('express');
 const router = express.Router();
+const createError = require('http-errors');
+const { promisify } = require('util');
+const { writeFile, readFile, access } = require('fs');
+const { EOL } = require('os');
+
+const writeFilePromise = promisify(writeFile);
+const readFilePromise = promisify(readFile);
+
+/* Credentials*/
+const user = {};
+
+/* Contacts */
+let contactsList = []
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('home', { title: 'Home' });
+  delete user.username;
+  delete user.password;
+  console.log(user)
+  res.render('home', { title: 'home' });
 });
 
-/* GET about page. */
-router.get('/about', (req, res, next) => {
-  res.render('about', { title: 'About' });
+/* GET others pages. */
+router.get('/:page', ({params: { page }}, res, next) => {
+  res.render(page, { title: page });
 });
 
-/* GET services listing. */
-router.get('/services', (req, res, next) => {
-  res.render('services', { title: 'Services' });
-});
+/* Middleware for authentication*/
+const requireAuthentication = (req, res, next) => {
+  if(!user.username || !user.password) {
+    res.redirect('../login')
+  }
+  next();
+}
 
-/* GET projects listing. */
-router.get('/projects', (req, res, next) => {
-  res.render('projects', { title: 'Projects' });
-});
+/* POST login page. */
+router.post('/login', ({ body: { username, password } }, res, next) => {
+  readFilePromise(`${__dirname}/../database.csv`, 'utf8')
+  .then(data => {
+    const headers = data.split(EOL)[0].split(', ');
+    const usersList = data.split(EOL).slice(1).map(row => {
+      const values = row.split(', ');
+      return values.reduce((user, value, index) => {
+        return {
+          ...user,
+          [headers[index]]: value
+        }
+      }, {})
+    });
 
-/* GET contact page. */
-router.get('/contact', (req, res, next) => {
-  res.render('contact', { title: 'Contact' });
-});
+    contactsList = usersList.filter(user => !user.Password)
+
+    const isAdmin = usersList.filter(user => {
+      return user.Password && user.ContactName === username && user.Password === password
+    }).length === 1;
+
+    if(isAdmin) {
+      user.username = username;
+      user.password = password;
+      res.redirect('users/business_contact_list');
+    } else {
+      res.redirect('login');
+    }
+  })
+  .catch(next)
+})
+
+/* Control auth */
+router.all('/users/*', requireAuthentication)
+
+/* GET business_contact_list page */
+router.get(`/users/business_contact_list`, (req , res, next) => {
+  res.render('business_contact_list', { title: 'business_contact_list', users: contactsList});
+})
 
 module.exports = router;
